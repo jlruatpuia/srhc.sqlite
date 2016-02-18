d7090869c002e82fa86d35dd04dc400b740d9839
jlruatpuia <Apuii@123>
2016-02-18 10:55:18 +0530

    Project Created

---
+++ SRHC/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace SRHC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainerControl = new DevExpress.XtraEditors.SplitContainerControl();
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.popupControlContainer2 = new DevExpress.XtraBars.PopupControlContainer(this.components);
+            this.buttonEdit = new DevExpress.XtraEditors.ButtonEdit();
+            this.popupControlContainer1 = new DevExpress.XtraBars.PopupControlContainer(this.components);
+            this.someLabelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.someLabelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.homeRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.helpRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.helpRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl)).BeginInit();
+            this.splitContainerControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupControlContainer2)).BeginInit();
+            this.popupControlContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupControlContainer1)).BeginInit();
+            this.popupControlContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainerControl
+            // 
+            this.splitContainerControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl.Location = new System.Drawing.Point(0, 144);
+            this.splitContainerControl.Name = "splitContainerControl";
+            this.splitContainerControl.Padding = new System.Windows.Forms.Padding(6);
+            this.splitContainerControl.Panel1.Text = "Panel1";
+            this.splitContainerControl.Panel2.Text = "Panel2";
+            this.splitContainerControl.Size = new System.Drawing.Size(1100, 525);
+            this.splitContainerControl.SplitterPosition = 165;
+            this.splitContainerControl.TabIndex = 0;
+            this.splitContainerControl.Text = "splitContainerControl1";
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.ApplicationButtonText = null;
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.MaxItemId = 62;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.homeRibbonPage,
+            this.helpRibbonPage});
+            this.ribbonControl.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl.Size = new System.Drawing.Size(1100, 144);
+            this.ribbonControl.StatusBar = this.ribbonStatusBar;
+            // 
+            // popupControlContainer2
+            // 
+            this.popupControlContainer2.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.popupControlContainer2.Appearance.Options.UseBackColor = true;
+            this.popupControlContainer2.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.popupControlContainer2.Controls.Add(this.buttonEdit);
+            this.popupControlContainer2.Location = new System.Drawing.Point(238, 289);
+            this.popupControlContainer2.Name = "popupControlContainer2";
+            this.popupControlContainer2.Ribbon = this.ribbonControl;
+            this.popupControlContainer2.Size = new System.Drawing.Size(118, 28);
+            this.popupControlContainer2.TabIndex = 3;
+            this.popupControlContainer2.Visible = false;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.EditValue = "Some Text";
+            this.buttonEdit.Location = new System.Drawing.Point(3, 5);
+            this.buttonEdit.MenuManager = this.ribbonControl;
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.buttonEdit.Size = new System.Drawing.Size(100, 20);
+            this.buttonEdit.TabIndex = 0;
+            // 
+            // popupControlContainer1
+            // 
+            this.popupControlContainer1.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.popupControlContainer1.Appearance.Options.UseBackColor = true;
+            this.popupControlContainer1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.popupControlContainer1.Controls.Add(this.someLabelControl2);
+            this.popupControlContainer1.Controls.Add(this.someLabelControl1);
+            this.popupControlContainer1.Location = new System.Drawing.Point(111, 197);
+            this.popupControlContainer1.Name = "popupControlContainer1";
+            this.popupControlContainer1.Ribbon = this.ribbonControl;
+            this.popupControlContainer1.Size = new System.Drawing.Size(76, 70);
+            this.popupControlContainer1.TabIndex = 2;
+            this.popupControlContainer1.Visible = false;
+            // 
+            // someLabelControl2
+            // 
+            this.someLabelControl2.Location = new System.Drawing.Point(3, 57);
+            this.someLabelControl2.Name = "someLabelControl2";
+            this.someLabelControl2.Size = new System.Drawing.Size(49, 13);
+            this.someLabelControl2.TabIndex = 0;
+            this.someLabelControl2.Text = "Some Info";
+            // 
+            // someLabelControl1
+            // 
+            this.someLabelControl1.Location = new System.Drawing.Point(3, 3);
+            this.someLabelControl1.Name = "someLabelControl1";
+            this.someLabelControl1.Size = new System.Drawing.Size(49, 13);
+            this.someLabelControl1.TabIndex = 0;
+            this.someLabelControl1.Text = "Some Info";
+            // 
+            // homeRibbonPage
+            // 
+            this.homeRibbonPage.Name = "homeRibbonPage";
+            this.homeRibbonPage.Text = "Home";
+            // 
+            // helpRibbonPage
+            // 
+            this.helpRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.helpRibbonPageGroup});
+            this.helpRibbonPage.Name = "helpRibbonPage";
+            this.helpRibbonPage.Text = "Help";
+            // 
+            // helpRibbonPageGroup
+            // 
+            this.helpRibbonPageGroup.Name = "helpRibbonPageGroup";
+            this.helpRibbonPageGroup.Text = "Help";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 669);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbonControl;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1100, 31);
+            // 
+            // Form1
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 700);
+            this.Controls.Add(this.splitContainerControl);
+            this.Controls.Add(this.popupControlContainer1);
+            this.Controls.Add(this.popupControlContainer2);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbonControl);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl)).EndInit();
+            this.splitContainerControl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupControlContainer2)).EndInit();
+            this.popupControlContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupControlContainer1)).EndInit();
+            this.popupControlContainer1.ResumeLayout(false);
+            this.popupControlContainer1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonPage homeRibbonPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPage helpRibbonPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup helpRibbonPageGroup;
+        private DevExpress.XtraBars.PopupControlContainer popupControlContainer1;
+        private DevExpress.XtraEditors.LabelControl someLabelControl2;
+        private DevExpress.XtraEditors.LabelControl someLabelControl1;
+        private DevExpress.XtraBars.PopupControlContainer popupControlContainer2;
+        private DevExpress.XtraEditors.ButtonEdit buttonEdit;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+
+    }
+}
